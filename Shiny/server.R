1f90d7c5cc512fbe02689552590b642a99f51d80
krozic <kyle.rozic@gmail.com>
2021-09-23 04:53:31 -0400

    main

---
+++ Shiny/server.R
@@ -0,0 +1,18 @@
+library(shiny)
+source('algorithms.R')
+
+shinyServer(function(input, output) {
+    predictedWord <- reactive({predict_word(input$Query)})
+    predictedWords <- reactive({predict_words(input$Query)})
+    
+    output$predictedWord <- renderText(
+        paste('The word with the highest probability is:', predictedWord()$pred)
+        )
+    
+    output$predictedWords <- renderDataTable(predictedWords(),
+                                    options = list(
+                                        paging = FALSE,
+                                        searching = FALSE
+                                    )
+    )
+})
